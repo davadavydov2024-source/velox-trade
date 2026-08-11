e7ba7dae5be8365fbc13b757eb2c90f564927224
davadavydov2024-source <davadavydov2024@gmail.com>
2026-08-11 12:34:31 +0300

    Configurable closing screens: maintenance, support, topup, 404

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import { Providers } from "./providers";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
-import { AdBanner } from "@/components/AdBanner";
 import { MobileTabBar } from "@/components/MobileTabBar";
 
 export const metadata: Metadata = {
@@ -30,7 +29,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="bg-bg text-white min-h-screen flex flex-col">
         <Providers>
           <Header />
-          <AdBanner />
           <main className="flex-1 pb-16 lg:pb-0">{children}</main>
           <Footer />
           <MobileTabBar />
